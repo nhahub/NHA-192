f37d4a7a10d52e6c0743a6a0d726d5905047c16a
yasmeinsaeid55-ops <yasmeinsaeid55@gmail.com>
2025-11-20 21:37:47 +0200

    test implementations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
+    testImplementation(libs.junit.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
---
+++ app/src/androidTest/java/com/example/MovieApp/database/MoviesDataBaseTest.kt
@@ -0,0 +1,18 @@
+package com.example.MovieApp.database
+
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+
+class MoviesDataBaseTest {
+    @Before
+    fun setUp() {
+        TODO("Not yet implemented")
+    }
+
+    @After
+    fun tearDown() {
+        TODO("Not yet implemented")
+    }
+
+}
